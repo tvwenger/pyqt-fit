4966aa8e858f28ae0edc4e95a0881845aae74ebd
Trey Wenger <tvwenger@gmail.com>
2020-02-22 11:58:50 -0800

    remove distribute requirement

--- setup.py
+++ setup.py
@@ -32,8 +32,7 @@ setup(name='PyQt-Fit',
                                  ]
                     },
       scripts=['bin/pyqt_fit1d.py'],
-      install_requires=['distribute >=0.6',
-                        'numpy >=1.5.0',
+      install_requires=['numpy >=1.5.0',
                         'scipy >=0.10.0',
                         'matplotlib',
                         'path.py >=2.4.1'
